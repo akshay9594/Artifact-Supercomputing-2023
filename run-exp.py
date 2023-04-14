334d79ecfa51717d2adcdfc92ae55c93164c938c
Akshay <akshay@pop-os.localdomain>
2023-04-14 10:47:02 -0400

    run-exp with individual input support complete

--- run-exp.py
+++ run-exp.py
@@ -36,6 +36,8 @@ def compile_UA(baseline,base_path,input_class):
         make_result = Popen(['make','CLASS='+input_class],stdout=PIPE,stderr=PIPE)
     elif(baseline == '2'):
         make_result = Popen('make',stdout=PIPE,stderr=PIPE)
+    else:
+        make_result = Popen('make',stdout=PIPE,stderr=PIPE)
     
     output, err = make_result.communicate()
     output = str(output, 'UTF-8')
@@ -44,17 +46,15 @@ def compile_UA(baseline,base_path,input_class):
     if(('error:' in output) or ('error:' in error)):
         print("Compilation failed...")
         exit()
-    else:
-        print("Compilation successfull...")
     return
 
-
+#Executing the amgmk application
 def execute_amgmk(iters):
     app_times = []
     loop_times = []
 
     cores = 0
-    for i in range(1,iters):
+    for i in range(0,iters):
         exec_result = Popen('./AMGMk',stdout=PIPE,stderr=PIPE)
         output, err_val = exec_result.communicate()
                 
@@ -69,7 +69,7 @@ def execute_amgmk(iters):
             loop_time = [float(i) for i in re.findall("\d+\.\d+", search.group(1))]
             loop_times.append(loop_time.pop())
 
-            if(i == 1):
+            if(i == 0):
                 search = re.search('max_num_threads =(.*)', output)
                 cores = [int(s) for s in re.findall(r'\b\d+\b', search.group(1))].pop()
             
@@ -83,7 +83,39 @@ def execute_amgmk(iters):
     Popen(['make','clean'],stdout=PIPE,stderr=PIPE)
     return (app_time_sum/iters,loop_time_sum/iters,cores)
 
+#Executing the UA application
+def execute_UA(exec_path,input_class,iters):
+    transf_times = []
+    os.chdir(exec_path)
 
+    exec_command = './ua.'+input_class+'.x'
+    cores = 0
+    for i in range(0,iters):
+        exec_result = Popen(exec_command,stdout=PIPE,stderr=PIPE)
+        output, err_val = exec_result.communicate()
+                
+        output = str(output,'UTF-8')
+       
+        if(isinstance(output,str)):
+            search = re.search('Transf total Time=(.*)seconds', output)
+            wall_time = [float(i) for i in re.findall("\d+\.\d+", search.group(1))]
+            transf_times.append(wall_time.pop())
+
+            if(i == 0):
+                search = re.search('max_num_threads =(.*)', output)
+                cores = [int(s) for s in re.findall(r'\b\d+\b', search.group(1))].pop()
+            
+    transf_time_sum = 0.0
+    for i in range(0,len(transf_times)):
+        transf_time_sum += transf_times[i]
+   
+    return (transf_time_sum/iters,cores)
+
+
+#Selecting the base line:
+#There are two possible baselines -
+#(1) The serial code
+#(2) The Cetus output code (Without subscripted subscript analysis of the technique applied)
 def select_baseline():
 
     baseline = input("\n\t1.Serial\n\t2.Cetus-Output-WithoutSubSub\n\t->Select the baseline for comparison:\n")
@@ -98,7 +130,7 @@ def select_baseline():
     return baseline
 
 
-
+#Running the experiment with amgmk benchmark
 def run_exp_amgmk(base_path,opt_code_path,iters):
 
     print("\nCompiling and Running the baseline code", iters,"times and displaying the execution time...") 
@@ -122,9 +154,45 @@ def run_exp_amgmk(base_path,opt_code_path,iters):
     print("->Cores used=",cores)
 
 
+#Running the experiment with the UA benchmark
 def run_exp_UA(baseline,base_path,opt_code_path,input_class,iters):
-    print("\nCompiling and Running the baseline code", iters,"times and displaying the execution time...") 
-    compile_UA(baseline,base_path,input_class)
+
+    print("\nCompiling and Running the codes", iters,"times and displaying the execution time...")
+
+    #Change pth to the appropriate directory which has the baseline code
+    if(baseline == '1'):
+        compile_path = base_path + 'UA'
+        exec_path = base_path +'bin'
+    elif(baseline == '2'):
+        compile_path = base_path + 'CLASS-'+input_class
+        exec_path = compile_path
+
+    #Compile and execute the baseline code
+    compile_UA(baseline,compile_path,input_class)
+    avg_base_time,cores = execute_UA(exec_path,input_class,iters)
+
+    print("\n----------------------Timing Results for the Transf routine in UA------------------------\n")
+    print("->Average baseline subroutine execution time=", avg_base_time, " s")
+    
+    #Clean the baseline object files
+    os.chdir(compile_path)
+    Popen(['make','clean'],stdout=PIPE,stderr=PIPE)
+
+    #Change to the directory which has the optimized code (Parallel code with technique applied)
+    compile_path = opt_code_path
+    exec_path = compile_path
+
+    #Compile and execute the code
+    compile_UA(baseline,compile_path,input_class)
+    avg_opt_time,cores = execute_UA(exec_path,input_class,iters) 
+    
+    print("->Average subroutine execution time of Cetus Parallelized Code (with technique applied)=", avg_opt_time, " s")
+    print("->Subroutine Speedup=",avg_base_time/avg_opt_time)
+    print("->Cores used=",cores)
+
+    #Clean the optimized code object files
+    os.chdir(compile_path)
+    Popen(['make','clean'],stdout=PIPE,stderr=PIPE)
 
 
 val = input("\t1. amgmk-v1.0\n\t2. UA-NAS\n\t->Select the benchmark you want to test by entering the number:\n")
@@ -187,12 +255,14 @@ elif(val == '2'):
         input_class = input("Enter the Input Class (A,B,C or D) to use:")
 
         if(baseline == '1'):
-            base_path = root+'/UA-NAS/Baselines/Serial/UA'
+            base_path = root+'/UA-NAS/Baselines/Serial/'
             
         elif(baseline == '2'):
-            base_path = root+'/UA-NAS/Baselines/Cetus-Output-WithoutSubSub/CLASS-'+input_class
+            base_path = root+'/UA-NAS/Baselines/Cetus-Output-WithoutSubSub/'
         
         iters = 3
+        opt_code_path += 'CLASS-'+input_class
+
         run_exp_UA(baseline,base_path,opt_code_path,input_class,iters)
 
         os.chdir(root)
